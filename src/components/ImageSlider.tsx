8553f0c8119e0ff7b81b551c2e9520bba42f7914
Edijs Mezits <edijs.mezits@gmail.com>
2024-02-05 20:32:27 +0200

    Cart, productID page

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -7,6 +7,8 @@ import "swiper/css/pagination";
 import type SwiperType from "swiper";
 import { Pagination } from "swiper/modules";
 import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 type ImageSliderProps = {
   urls: string[];
@@ -29,7 +31,7 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
         isEnd: activeIndex === (urls.length ?? 0) - 1,
       });
     });
-  });
+  }, [swiper, urls]);
 
   const activeStyles =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
@@ -38,11 +40,40 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
   return (
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
-        <button></button>
-        <button></button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slideNext();
+          }}
+          className={cn(activeStyles, "right-3 transition", {
+            [inactiveStyles]: slideConfig.isEnd,
+            "hover:bg-primary-300 text-pirmary-800 opacity-100": !slideConfig.isEnd,
+          })}
+          aria-label="next image"
+        >
+          <ChevronRight className="h-4 w-4 text-zinc-700" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slidePrev();
+          }}
+          className={cn(activeStyles, "left-3 transition", {
+            [inactiveStyles]: slideConfig.isBeggining,
+            "hover:bg-primary-300 text-primary-800 opacity-100": !slideConfig.isBeggining,
+          })}
+          aria-label="previous image"
+        >
+          <ChevronLeft className="h-4 w-4 text-zinc-700" />
+        </button>
       </div>
 
       <Swiper
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="rounded-full transition ${className}"></span>`;
+          },
+        }}
         onSwiper={(swiper) => setSwiper(swiper)}
         modules={[Pagination]}
         spaceBetween={50}

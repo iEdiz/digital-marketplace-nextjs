b96b6e467b8cd3b72cf004e4b4ce9505de98af7d
Edijs Mezits <edijs.mezits@gmail.com>
2024-02-09 17:25:00 +0200

    changes

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -6,12 +6,12 @@ import { trpc } from "@/trpc/client";
 import Link from "next/link";
 import ProductListing from "./ProductListing";
 
-interface ProductReelProps {
+type ProductReelProps = {
   title: string;
   subtitle?: string;
   href?: string;
   query: TQueryValidator;
-}
+};
 
 const FALLBACK_LIMIT = 4;
 
@@ -20,11 +20,12 @@ const ProductReel = (props: ProductReelProps) => {
 
   const { data: queryResults, isLoading } = trpc.getInfiniteProducts.useInfiniteQuery(
     {
+      // HOW MANY PRODUCTS WILL GET FETCHED
       limit: query.limit ?? FALLBACK_LIMIT,
       query,
     },
     {
-      getNextPageParam: (lastPage) => lastPage.nextPage,
+      getNextPageParam: (lastpage) => lastpage.nextPage,
     }
   );
 
@@ -39,15 +40,15 @@ const ProductReel = (props: ProductReelProps) => {
 
   return (
     <section className="py-12">
-      <div className="md:flex md:items-center md:justify-between mb-4">
+      <div className="md: flex md:items-center md:justify-between mb-4">
         <div className="max-w-2xl px-4 lg:max-w-4xl lg:px-0">
-          {title ? <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">{title}</h1> : null}
+          {title ? <h1 className="text-2xl font-bold text-grey-900 sm:text-3xl">{title}</h1> : null}
           {subtitle ? <p className="mt-2 text-sm text-muted-foreground">{subtitle}</p> : null}
         </div>
 
         {href ? (
           <Link href={href} className="hidden text-sm font-medium text-blue-600 hover:text-blue-500 md:block">
-            Shop the collection <span aria-hidden="true">&rarr;</span>
+            Check out the collection <span aria-hidden="true">&rarr;</span>
           </Link>
         ) : null}
       </div>
@@ -55,8 +56,8 @@ const ProductReel = (props: ProductReelProps) => {
       <div className="relative">
         <div className="mt-6 flex items-center w-full">
           <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
-            {map.map((product, i) => (
-              <ProductListing key={`product-${i}`} product={product} index={i} />
+            {map.map((product, index) => (
+              <ProductListing key={`product-${index}`} product={product} index={index} />
             ))}
           </div>
         </div>
